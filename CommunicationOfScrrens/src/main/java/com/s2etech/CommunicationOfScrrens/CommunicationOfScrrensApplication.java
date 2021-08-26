e4e94b1c55f08fc1904b9cb0f71adea8280a825a
stsaginala <stsaginala@taxilla.com>
2021-08-26 16:00:39 +0530

    CommunicationOfScrrens

---
+++ CommunicationOfScrrens/src/main/java/com/s2etech/CommunicationOfScrrens/CommunicationOfScrrensApplication.java
@@ -0,0 +1,13 @@
+package com.s2etech.CommunicationOfScrrens;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CommunicationOfScrrensApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CommunicationOfScrrensApplication.class, args);
+	}
+
+}
---
+++ CommunicationOfScrrens/src/test/java/com/s2etech/CommunicationOfScrrens/CommunicationOfScrrensApplicationTests.java
@@ -0,0 +1,13 @@
+package com.s2etech.CommunicationOfScrrens;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CommunicationOfScrrensApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
